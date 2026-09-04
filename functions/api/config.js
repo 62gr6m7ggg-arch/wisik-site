2999340061535df39ea3d91a7b9cfe4ed0a6f73a
Edwin^i*2kpi <62gr6m7ggg@privaterelay.appleid.com>
2026-09-04 11:40:20 +0200

    Gebruik gratis Kladblokconfiguratie zonder betaalde e-mail-API

--- functions/api/config.js
+++ functions/api/config.js
@@ -1,17 +1,15 @@
 export async function onRequestGet({ env }) {
   const feedbackEnabled = Boolean(
     env.TURNSTILE_SITE_KEY &&
-    env.TURNSTILE_SECRET_KEY &&
-    env.CF_ACCOUNT_ID &&
-    env.EMAIL_API_TOKEN &&
-    env.FEEDBACK_TO &&
-    env.FEEDBACK_FROM
+    env.TURNSTILE_SECRET_KEY
   );
 
   return Response.json(
     {
       feedbackEnabled,
-      turnstileSiteKey: feedbackEnabled ? env.TURNSTILE_SITE_KEY : ""
+      turnstileSiteKey: feedbackEnabled ? env.TURNSTILE_SITE_KEY : "",
+      provider: feedbackEnabled ? "formsubmit" : "",
+      providerRetentionDays: feedbackEnabled ? 30 : null
     },
     {
       headers: {
